293771055007edbebb00cd57015cd5e882786179
Christiaan V <cvolschenk19@gmail.com>
2020-03-27 15:14:08 +0200

    Initial commit

---
+++ app.R
@@ -0,0 +1,157 @@
+library(shiny)
+library(tidyverse)
+library(shinydashboard)
+library(shinyjs)
+library(stringr)
+library(scales)
+
+style.plot <- function(gg, color) {
+  gg +
+    geom_line(size = 1) +
+    theme_minimal() +
+    theme(text = element_text(size = 20), legend.title = element_blank()) +
+    theme(legend.position = "none") +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    scale_x_date(
+      date_minor_breaks = "1 day",
+      date_labels = "%b %d"
+    ) +
+    xlab(NULL) +
+    ylab(NULL) +
+    scale_color_manual(values = c("1" = color))
+}
+
+kOrange <- "#ff851b"
+kGreen <-  "#00a65a"
+kRed <-    "#dd4b39"
+
+header <- dashboardHeader(title = "Coronavirus South Africa", titleWidth = "300")
+sidebar <- dashboardSidebar(disable = T)
+body <- dashboardBody(
+  tags$head(
+    # tags$meta(charset="UTF-8"),
+    # tags$meta(name="description", content="..."),
+    # tags$meta(name="keywords", content="..."),
+    # tags$meta(name="viewport", content="width=device-width, initial-scale=1.0"),
+    tags$link(rel="shortcut icon", href="https://cdn.countryflags.com/thumbs/south-africa/flag-round-250.png")
+  ),
+  # Open
+  box(
+    width = 4,
+    column(
+      width = 12,
+      valueBoxOutput("open_val", width = 12),
+    ),
+    column(
+      width = 12,
+      plotOutput("open_plot")
+    )
+
+  ),
+
+  # Recovered
+  box(
+    width = 4,
+    column(
+      width = 12,
+      valueBoxOutput("rec_val", width = 12),
+    ),
+    column(
+      width = 12,
+      plotOutput("rec_plot")
+    )
+
+  ),
+  #Deaths
+  box(
+    width = 4,
+    column(
+      width = 12,
+      valueBoxOutput("d_val", width = 12),
+    ),
+    column(
+      width = 12,
+      plotOutput("d_plot")
+    )
+
+  )
+)
+
+ui <- dashboardPage(header, sidebar, body, "Coronavirus South Africa", "green")
+
+server <- function(input, output, session) {
+
+  session$allowReconnect(TRUE)
+
+  # South Africa Time Series Data
+  getSATS <- reactive({
+    read.csv("sa_ts.csv") %>%
+      mutate(Date = as.Date(paste0(
+        "20",
+        substr(Date, 7, 8),
+        "-",
+        substr(Date, 1,2),
+        "-",
+        substr(Date, 4, 5)
+      ))) %>%
+      filter(!duplicated(Date)) %>%
+      arrange(Date)
+  })
+
+  # Values ------------------------------------------------------------------
+
+  # Open
+  output$open_val <- renderValueBox({
+    val <- tail(getSATS(), 1)$Open
+    val <- comma(val)
+    valueBox(val, "Open Cases", icon("stethoscope"), "orange", width = 12)
+  })
+
+  # Recovered
+  output$rec_val <- renderValueBox({
+    val <- tail(getSATS(), 1)$Recovered
+    val <- comma(val)
+    valueBox(val, "Recovered", icon("clipboard-check"), "green", width = 12)
+  })
+
+  #Deaths
+  output$d_val <- renderValueBox({
+    val <- tail(getSATS(), 1)$Deaths
+    val <- comma(val)
+    valueBox(val, "Deaths", icon("exclamation"), "red", width = 12)
+  })
+
+
+  # Plots -------------------------------------------------------------------
+
+  # Open
+  output$open_plot <- renderPlot({
+    df <- getSATS()
+
+    df %>%
+      mutate(`Open Cases` = Open) %>%
+      ggplot(aes(x = Date, y = `Open Cases`, color = "1")) %>%
+      style.plot(kOrange)
+  })
+
+  # Recovered
+  output$rec_plot <- renderPlot({
+    df <- getSATS()
+
+    df %>%
+      ggplot(aes(x = Date, y = Recovered, color = "1")) %>%
+      style.plot(kGreen)
+  })
+
+  #Deaths
+  output$d_plot <- renderPlot({
+    df <- getSATS()
+
+    df %>%
+      ggplot(aes(x = Date, y = Deaths, color = "1")) %>%
+      style.plot(kRed) +
+      scale_y_continuous(limits = c(0,4))
+  })
+}
+
+shinyApp(ui = ui, server = server, options = c("port" = 3838))
